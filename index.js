9ec7c5527470ed4ff3e257ed0b4d58d2315a4612
Rifki Salim <contact@rifkisalim.com>
2023-10-16 11:26:59 +0530

    🎉 init(project): initial commit

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+// Imports
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 
+// Init App
 var app = express();
 
+// Enable CORS
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+// Static File Server
+app.use("/public", express.static(process.cwd() + "/public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
-
-
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("File Metadata Microservice is listening on port " + port);
 });
